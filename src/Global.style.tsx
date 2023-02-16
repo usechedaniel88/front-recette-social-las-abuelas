377cd1d5361d4d8628865ec9c93c45e8314c2ab5
Daniel <usechedaniel88@gmail.com>
2023-02-16 11:17:49 +0100

    colores y fuentes definidas

--- src/Global.style.tsx
+++ src/Global.style.tsx
@@ -1,10 +1,25 @@
 import styled, { createGlobalStyle } from "styled-components";
 
+export const theme = {
+  green: '#46CC8D', 
+  background: 'rgba(176, 231, 201, 0.11)', 
+  red: '#FB2B3A',
+  gray: '#B7B7B7',  
+}
 
 export const GlobalStyles = createGlobalStyle`
 
-body{
-  background-color: #c4c4c4;
+@import url('https://fonts.googleapis.com/css2?family=Manjari:wght@100;400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Allura&family=Noto+Sans+KR&family=Poppins:wght@100&display=swap');
+
+*{
+  margin:0; 
+  padding: 0; 
+  box-sizing: border-box; 
+  font-family: 'Manjari', sans-serif;    
 }
 
+body{
+  background-color: ${theme.background};
+}
 `
