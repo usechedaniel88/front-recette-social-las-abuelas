f61f665ba600c6e44bc5503db1c64bbaae9ab2a2
Daniel <usechedaniel88@gmail.com>
2023-02-21 19:10:55 +0100

    ajuste de tamaño letra

--- src/pages/StartPage/startPage.style.tsx
+++ src/pages/StartPage/startPage.style.tsx
@@ -1,9 +1,5 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
-import { GlobalStyles } from "../../Global.style"
-
-
-
 
 export const Container = styled.div`
     position: relative;
@@ -30,51 +26,16 @@ export const DivButton = styled.div`
     font-size: 15px;
     text-align: center;
     padding: 20px 0;
-    bottom: 5%;
+    bottom: 0%;
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: 1;
 `
 export const H1Text = styled.h1`
-@import url('https://fonts.googleapis.com/css2?family=Allura:wght@100&display=swap');
-
 font-family: 'Allura', cursive;
+font-size: 2rem;
 `
 export const H2Text = styled.h2`
-@import url('https://fonts.googleapis.com/css2?family=Manjari:wght@100;400;700&display=swap');
-
 font-family: 'Manjari', sans-serif;
+font-size: 2rem;
 `
-
-// export const Container = styled.div`
-// height: 100vh;
-// width: auto;
-// `
-
-// export const VideoBg = styled.video`
-// width: 100%;
-// height: 100%;
-// object-fit: cover;
-// position: absolute;
-// z-index: -1;
-// `
-
-// export const StyledLink = styled(Link)`
-// text-decoration: none;
-// `
-// export const DivButton = styled.div`
-// background-color: #FB2B3A; 
-// color: white;
-// width: 95%;
-// padding: 5%;
-// border-radius: 15px;
-// border: 5px solid white;
-// position: relative;
-// margin: 0 auto;
-// text-align: center;
-// z-index: 1;
-// `
-// export const ButtonH2 = styled.h2`
-// font-family: 'Allura', cursive;
-// color: white;
-// `
