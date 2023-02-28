c60146ef003f8cec7849bfccfd200a092851cb0d
Fabio Migliaccio <febbio9@gmail.com>
2023-02-28 14:35:45 +0100

    modify submit okbutton

--- src/components/OkButton/OkButton.tsx
+++ src/components/OkButton/OkButton.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import TickImage from '../../assets/icons/tick-white.png'
-import { ButtonContainer, PText } from './okButton.style'
+import { InputSubmit, PText } from './okButton.style'
 
 type Props = {
     title: string
 }
 
 const OkButton = (props: Props) => {
-  return (
-    <Link to={'/profile'}>
-    <ButtonContainer>
-        <img src={TickImage} alt="Imagen ok" />
-        <PText>{props.title}</PText>
-    </ButtonContainer>
-    </Link>
+  return ( 
+
+   <InputSubmit type='submit' value='Submit!' />
+    // <Link to={'/profile'}>
+    // <ButtonContainer>
+    //     <img src={TickImage} alt="Imagen ok" />
+    //     <PText>{props.title}</PText>
+    // </ButtonContainer>
+    // </Link>
   )
 }
 
--- src/components/OkButton/okButton.style.tsx
+++ src/components/OkButton/okButton.style.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { theme } from "../../Global.style";
 
 
-export const ButtonContainer = styled.div`
+export const InputSubmit = styled.input`
 width: fit-content;
 display: flex;
 flex-direction: row;
@@ -10,10 +10,13 @@ align-items: center;
 background-color: ${theme.red};
 border-radius: 25px;
 color: #FFFFFF;
-padding: 6px 20px;
+padding: 10px 20px 10px 20px;
 font-size: 20px;
 margin: 0 auto  ;
 margin-top: 20px;
+border:none;
+
+
 
 
 `
