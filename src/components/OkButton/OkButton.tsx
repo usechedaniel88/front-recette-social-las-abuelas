7cb36d2dbd774d30cb3936dc4d8308486a1d81e4
Fabio Migliaccio <febbio9@gmail.com>
2023-03-01 11:12:46 +0100

    adjust post recipe method

--- src/components/OkButton/OkButton.tsx
+++ src/components/OkButton/OkButton.tsx
@@ -4,14 +4,15 @@ import TickImage from '../../assets/icons/tick-white.png'
 import { InputSubmit, PText } from './okButton.style'
 
 type Props = {
-    title: string
+    
     onClick: any
+    
 }
 
-const OkButton = (props: Props) => {
+const OkButton = ({ onClick}: Props ) => {
   return ( 
 
-   <InputSubmit type='submit' value='Submit!' />
+   <InputSubmit type='submit' value='Submit!' onClick={onClick} />
     // <Link to={'/profile'}>
     // <ButtonContainer>
     //     <img src={TickImage} alt="Imagen ok" />
--- src/components/RecipeForm.tsx
+++ src/components/RecipeForm.tsx
@@ -7,31 +7,34 @@ import axios from 'axios';
  
 
 function RecipeForm( ) {
-    const [title, setMsg] = useState('')
-    const [description, Setdescription] = useState('')
+    const [title, setTitle] = useState('')
+    const [description, SetDescription] = useState('')
     const [time, setTime] = useState('')
     const [ingredient, setIngredient] = useState('')
     const [quantity, setQuantity] = useState('')
     const [privacy, setPrivacy] = useState('')
-    let [allForm, setAllForm] = useState('')
-    
+    // let [allForm, setAllForm] = useState('')
+
 
      
     const submit = async (e: any) => {  
         e.preventDefault();
-        allForm = {
+        const allForm = {
          title: title, 
          description: description,
          time: time,
-         ingredient: ingredient,
-         quantity: quantity,
-         privacy: privacy
+         ingredients: ingredient,
+         qty: quantity,
+         is_private: privacy
 
         }
+  
 
         try{ 
-
-            await axios.post('http://localhost:3000/', {allForm})
+console.log(allForm)
+         const result =   await axios.post('http://localhost:4000/api/v1/recipes/', {allForm})
+           console.log(result)
+          //if (result == recipe) 
         }
         catch(e){
            console.log(e);
@@ -42,13 +45,13 @@ function RecipeForm( ) {
     return (
         <Container>
 
-            <Form action="POST">
+            <Form>
 
                 <h4>Create a recipe:</h4>
                 
-                <Input type="text" placeholder="Recipe names" onChange={ e => {setMsg(e.target.value)}}/>
+                <Input type="text" placeholder="Recipe names"  name='title' onChange={ e => {setTitle(e.target.value)}}/>
 
-                <InputTextArea type="text" placeholder="Instructions" onChange={ e => {Setdescription(e.target.value)}}/>
+                <InputTextArea type="text" placeholder="Instructions" onChange={ e => {SetDescription(e.target.value)}}/>
 
                 <label><InputTime type="number" placeholder="Time to prepare" min='1' onChange={ e => {setTime(e.target.value)}} />min</label>
 
@@ -70,8 +73,9 @@ function RecipeForm( ) {
                 <label><InputQuantity type="number" placeholder="Quantity" onChange={ e => {setQuantity(e.target.value)}}/>g</label>
 
                 <Input type="search" placeholder="Search ingredient by name" onChange={ e => {setIngredient(e.target.value)}} />
-                <OkButton    onClick={submit} title=''
+                <OkButton    onClick={submit} 
                            />
+
             </Form>
 
         </Container>
@@ -80,3 +84,24 @@ function RecipeForm( ) {
 }
 
 export default RecipeForm
+
+
+
+const App = () => {
+    const [username, setUsername] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUsername(e.target.elements.form1.value)
+        axios.post('http://localhost:3000/api', { name: username })
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="form1" placeholder="Enter name:" />
+                <button type="submit">Submit</button>
+            </form>
+        </div >
+    )
+}
